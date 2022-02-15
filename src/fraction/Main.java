b6439258c317f75025d315ef9969d15b13e21bd6
sikorskii <rokmac96@gmail.com>
2022-02-15 08:23:06 +0300

    task1

--- src/fraction/Main.java
+++ src/fraction/Main.java
@@ -1,8 +1,66 @@
 package fraction;
 
-import java.util.*;
-
 import static fraction.SimpleFraction.*;
+
+class SimpleFraction {
+    private double numerator = 1;
+    private double denominator = 1;
+
+    public SimpleFraction() {
+
+    }
+
+    public SimpleFraction(double numerator, double denominator) {
+        if (denominator == 0) {
+            throw new ArithmeticException("Fraction needs denominator != 0");
+        }
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+
+    public static SimpleFraction addFractions(SimpleFraction a, SimpleFraction b) {
+        return new SimpleFraction(a.numerator * b.denominator + b.numerator * a.denominator,
+                a.denominator * b.denominator);
+    }
+
+    public static SimpleFraction multiplyFractions(SimpleFraction a, SimpleFraction b) {
+        return new SimpleFraction(a.numerator * b.numerator, a.denominator * b.denominator);
+    }
+
+    public static SimpleFraction divideFractions(SimpleFraction a, SimpleFraction b) {
+        return new SimpleFraction(a.numerator * b.denominator, a.denominator * b.numerator);
+    }
+
+    public static SimpleFraction subtractFractions(SimpleFraction a, SimpleFraction b) {
+        return new SimpleFraction(a.numerator * b.denominator - b.numerator * a.denominator,
+                a.denominator * b.denominator);
+    }
+
+    public SimpleFraction addFraction(SimpleFraction b) {
+        return new SimpleFraction(this.numerator * b.denominator + b.numerator * this.denominator,
+                this.denominator * b.denominator);
+    }
+
+    public SimpleFraction multiplyFraction(SimpleFraction b) {
+        return new SimpleFraction(this.numerator * b.numerator, this.denominator * b.denominator);
+    }
+
+    public SimpleFraction divideFraction(SimpleFraction b) {
+        return new SimpleFraction(this.numerator * b.denominator, this.denominator * b.numerator);
+    }
+
+    public SimpleFraction subtractFraction(SimpleFraction b) {
+        return new SimpleFraction(this.numerator * b.denominator - b.numerator * this.denominator,
+                this.denominator * b.denominator);
+    }
+
+    @Override
+    public String toString() {
+        return numerator + "/" + denominator;
+    }
+}
+
 
 public class Main {
     public static void main(String[] args) {
@@ -11,6 +69,10 @@ public class Main {
         SimpleFraction c = new SimpleFraction(30, 3);
         SimpleFraction d = new SimpleFraction(1, 2);
         SimpleFraction e = new SimpleFraction(2, 1);
+        SimpleFraction f = new SimpleFraction();
+
+        SimpleFraction example0 = addFractions(f, f);
+        System.out.println(example0);
 
         SimpleFraction example1 = addFractions(a, b);
         System.out.println(example1);
